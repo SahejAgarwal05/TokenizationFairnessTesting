f4da096f9acb1900d7185c94b26d33a934d44900
vivalavida <barid.x.ai@gmail.com>
2025-05-06 13:57:12 +0800

    major

--- lm_eval_job.py
+++ lm_eval_job.py
@@ -3,14 +3,9 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
 import lm_eval
 from lm_eval.utils import setup_logging
 from lm_eval.loggers import WandbLogger
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
-import warnings
+from transformers import AutoTokenizer
 from model_main import LlamaPrunedModel
 import torch
-import torch.nn as nn
-
-from lm_eval import evaluator
 from lm_eval.models.huggingface import HFLM
 token = "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"
 
@@ -22,18 +17,17 @@ tokenizer = AutoTokenizer.from_pretrained(main_model_id,token)
 
 model = LlamaPrunedModel(main_model_id, small_model_id, compression_ratio,token=token)
 # initialize logging
+task_manager = lm_eval.tasks.TaskManager()
 setup_logging("DEBUG") # optional, but recommended; or you can set up logging yourself
 results = lm_eval.simple_evaluate( # call simple_evaluate
     model=HFLM(pretrained=model, tokenizer=model.tokenizer),
-    tasks=["mmlu_algebra"],
+    tasks=["mgsm"],
     num_fewshot=5,
     log_samples=True,
     # task_manager=task_manager,
 )
 
-wandb_logger = WandbLogger(
-    project="lm-eval-harness-integration", job_type="eval"
-)  # or empty if wandb.init(...) already called before
+wandb_logger = WandbLogger()
 wandb_logger.post_init(results)
 wandb_logger.log_eval_result()
 wandb_logger.log_eval_samples(results["samples"])  # if log_samples
