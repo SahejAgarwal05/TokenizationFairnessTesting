a96c09cf8adcd1fbf55049549b80a25cf463f27c
vivalavida <barid.x.ai@gmail.com>
2025-05-09 22:28:56 +0800

    major

--- cross_model/TokeniserMap.py
+++ cross_model/TokeniserMap.py
@@ -12,11 +12,10 @@ class TokenizerMap():
             if token_type in self.main_tokenizer.special_tokens_map:
                 map_dict[self.pruner_tokenizer.special_tokens_map[token_type]] = self.main_tokenizer.special_tokens_map[token_type]
         return map_dict
-    
+
     def map_string(self, string):
         for i in self.map_dict:
             while i in string:
                 string = string.replace(i, self.map_dict[i])
                 print(string)
         return string
-
