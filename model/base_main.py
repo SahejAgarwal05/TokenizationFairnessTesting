a8673916bc1795888b230716a190808dc86cf151
vivalavida <barid.x.ai@gmail.com>
2025-05-10 15:52:16 +0800

    major

--- model/base_main.py
+++ model/base_main.py
@@ -1,20 +1,10 @@
 import torch
 import torch.nn as nn
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
-from model.gemma2_prunner import TokenPruner as Gemma2TokenPruner
-from model.llama3_pruner import TokenPruner as Llama3TokenPruner
+from model.pruner_main import Pruner
 
 # import bitsandbytes as bnb
 from transformers import BitsAndBytesConfig
-from model.across_family_map import (
-    convert_aya_expanse_to_gemma2,
-    convert_aya_expanse_to_llama3,
-    convert_gemma2_to_llama3,
-    convert_gemma2_to_aya_expanse,
-    convert_llama3_to_gemma2,
-    convert_llama3_to_aya_expanse,
-)
-
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
@@ -52,66 +42,17 @@ class PrunedModel(nn.Module):
         self.across_family_flag = False
         self.across_family_forward_fn = lambda x: x
         self.across_family_backward_fn = lambda x: x
-        if "gemma" in small_model_id:
-            self.token_pruner = Gemma2TokenPruner(
-                small_model_id, compression_ratio, device="cuda:0"
-            )
-            if "llama" in main_model_id:
-                self.across_family_backward_fn = lambda x: convert_gemma2_to_llama3(
-                    x
-                )
-                self.across_family_forward_fn = lambda x: convert_llama3_to_gemma2(x)
-                self.across_family_flag = True
-            if "aya-expanse" in main_model_id:
-                self.across_family_backward_fn = (
-                    lambda x: convert_gemma2_to_aya_expanse(x)
-                )
-                self.across_family_forward_fn = lambda x: convert_aya_expanse_to_gemma2(
-                    x
-                )
-                self.across_family_flag = True
-        elif "llama" in small_model_id:
-            self.token_pruner = Llama3TokenPruner(
-                small_model_id, compression_ratio, device="cuda:0"
-            )
-            if "gemma" in main_model_id:
-                self.across_family_backward_fn = lambda x: convert_llama3_to_gemma2(
-                    x
-                )
-                self.across_family_forward_fn = lambda x: convert_gemma2_to_llama3(x)
-                self.across_family_flag = True
-            if "aya-expanse" in main_model_id:
-                self.across_family_backward_fn = (
-                    lambda x: convert_llama3_to_aya_expanse(x)
-                )
-                self.across_family_forward_fn = lambda x: convert_aya_expanse_to_llama3(
-                    x
-                )
-                self.across_family_flag = True
         self.compression_ratio = compression_ratio
+        self.post_tokenizer = Pruner(
+            small_model_id,
+            main_model_id,
+            compression_ratio,
+            device="cuda:0",
+        )
         # Freeze main model
         for param in self.main_model.parameters():
             param.requires_grad = False
         self.embeddings.requires_grad = False
-
-    def post_tokenizer(self, input_ids,attention_mask=None):
-        if self.across_family_flag:
-            org_tokens = self.main_tokenizer.batch_decode(
-                input_ids, skip_special_tokens=False
-            )
-            new_tokens = [self.across_family_forward_fn(o_t) for o_t in org_tokens]
-            new_inputs = self.pruner_tokenizer(
-                new_tokens, return_tensors="pt", add_special_tokens=False
-            )
-            input_ids = new_inputs["input_ids"]
-            attention_mask = new_inputs["attention_mask"]
-        pruned_tokens_ids, _ = self.token_pruner(input_ids.to("cuda:0"), attention_mask.to("cuda:0"))
-        pruned_tokens = self.pruner_tokenizer.batch_decode(
-            pruned_tokens_ids, skip_special_tokens=False
-        )
-        if self.across_family_flag:
-            pruned_tokens = [self.across_family_backward_fn(p_t) for p_t in pruned_tokens]
-        return pruned_tokens
 
     def forward(self, input_ids=None, attention_mask=None, **kwargs):
         if self.compression_ratio == 1.0:
---
+++ model/pruner_main.py
@@ -0,0 +1,82 @@
+from transformers import  AutoTokenizer
+from model.gemma2_prunner import TokenPruner as Gemma2TokenPruner
+from model.llama3_pruner import TokenPruner as Llama3TokenPruner
+from model.across_family_map import (
+    convert_aya_expanse_to_gemma2,
+    convert_aya_expanse_to_llama3,
+    convert_gemma2_to_llama3,
+    convert_gemma2_to_aya_expanse,
+    convert_llama3_to_gemma2,
+    convert_llama3_to_aya_expanse,
+)
+class Pruner:
+    def __init__(self, small_model_id, main_model_id, compression_ratio,device="cuda:0"):
+        self.main_model_id = main_model_id
+        self.small_model_id = small_model_id
+        self.device = device
+        self.pruner_tokenization = AutoTokenizer.from_pretrained(
+            small_model_id,
+            trust_remote_code=True,
+        )
+        self.main_tokenizer = AutoTokenizer.from_pretrained(
+            main_model_id,
+            trust_remote_code=True,
+        )
+        self.compression_ratio = compression_ratio
+        self.across_family_flag = False
+        self.across_family_forward_fn = lambda x: x
+        self.across_family_backward_fn = lambda x: x
+        if "gemma" in small_model_id:
+            self.token_pruner = Gemma2TokenPruner(
+                small_model_id, compression_ratio, device=self.device
+            )
+            if "llama" in main_model_id:
+                self.across_family_backward_fn = lambda x: convert_gemma2_to_llama3(
+                    x
+                )
+                self.across_family_forward_fn = lambda x: convert_llama3_to_gemma2(x)
+                self.across_family_flag = True
+            if "aya-expanse" in main_model_id:
+                self.across_family_backward_fn = (
+                    lambda x: convert_gemma2_to_aya_expanse(x)
+                )
+                self.across_family_forward_fn = lambda x: convert_aya_expanse_to_gemma2(
+                    x
+                )
+                self.across_family_flag = True
+        elif "llama" in small_model_id:
+            self.token_pruner = Llama3TokenPruner(
+                small_model_id, compression_ratio, device=self.device
+            )
+            if "gemma" in main_model_id:
+                self.across_family_backward_fn = lambda x: convert_llama3_to_gemma2(
+                    x
+                )
+                self.across_family_forward_fn = lambda x: convert_gemma2_to_llama3(x)
+                self.across_family_flag = True
+            if "aya-expanse" in main_model_id:
+                self.across_family_backward_fn = (
+                    lambda x: convert_llama3_to_aya_expanse(x)
+                )
+                self.across_family_forward_fn = lambda x: convert_aya_expanse_to_llama3(
+                    x
+                )
+                self.across_family_flag = True
+    def __call__(self, input_ids,attention_mask=None):
+        if self.across_family_flag:
+            org_tokens = self.main_tokenizer.batch_decode(
+                input_ids, skip_special_tokens=False
+            )
+            new_tokens = [self.across_family_forward_fn(o_t) for o_t in org_tokens]
+            new_inputs = self.pruner_tokenizer(
+                new_tokens, return_tensors="pt", add_special_tokens=False
+            )
+            input_ids = new_inputs["input_ids"]
+            attention_mask = new_inputs["attention_mask"]
+        pruned_tokens_ids, _ = self.token_pruner(input_ids.to("cuda:0"), attention_mask.to("cuda:0"))
+        pruned_tokens = self.pruner_tokenizer.batch_decode(
+            pruned_tokens_ids, skip_special_tokens=False
+        )
+        if self.across_family_flag:
+            pruned_tokens = [self.across_family_backward_fn(p_t) for p_t in pruned_tokens]
+        return pruned_tokens
