85aab42542468116ac6c10c2d2900b44bc877995
vivalavida <barid.x.ai@gmail.com>
2025-05-10 13:19:02 +0800

    major

--- batch_mgsm.sh
+++ batch_mgsm.sh
@@ -1,8 +1,11 @@
+# export CUDA_VISIBLE_DEVICES=3
+# python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=1.0 --task=mgsm
+# export CUDA_VISIBLE_DEVICES=3
+# python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.9 --task=mgsm
+# export CUDA_VISIBLE_DEVICES=3
+# python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.8 --task=mgsm
+# export CUDA_VISIBLE_DEVICES=3
+# python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.7 --task=mgsm
+
 export CUDA_VISIBLE_DEVICES=3
-python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=1.0 --task=mgsm
-export CUDA_VISIBLE_DEVICES=3
-python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.9 --task=mgsm
-export CUDA_VISIBLE_DEVICES=3
-python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.8 --task=mgsm
-export CUDA_VISIBLE_DEVICES=3
-python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.7 --task=mgsm
+python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.7 --task=arc
--- batch_multilingual_arc.sh
+++ batch_multilingual_arc.sh
@@ -1,8 +1,8 @@
-export CUDA_VISIBLE_DEVICES=1
-python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=1.0 --task=arc
-export CUDA_VISIBLE_DEVICES=1
-python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.9 --task=arc
-export CUDA_VISIBLE_DEVICES=1
+# export CUDA_VISIBLE_DEVICES=1
+# python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=1.0 --task=arc
+# export CUDA_VISIBLE_DEVICES=1
+# python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.9 --task=arc
+export CUDA_VISIBLE_DEVICES=2
 python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.8 --task=arc
-export CUDA_VISIBLE_DEVICES=1
-python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.7 --task=arc
+# export CUDA_VISIBLE_DEVICES=1
+# python lm_eval_job.py --small_model=meta-llama/Llama-3.2-1B-Instruct --main_model=meta-llama/Llama-3.1-8B-Instruct --compression_ratio=0.7 --task=arc
